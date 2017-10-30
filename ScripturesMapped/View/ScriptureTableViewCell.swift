4e6d39bffab1318d16850056a950f29a9e9cb196
victorlazaro <victor.mafralazaro@gmail.com>
2017-10-30 13:29:10 -0600

    Implemented up to the map

---
+++ ScripturesMapped/View/ScriptureTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  ScriptureTableViewCell.swift
+//  ScripturesMapped
+//
+//  Created by Victor Lazaro on 10/30/17.
+//  Copyright © 2017 Victor Lazaro. All rights reserved.
+//
+
+import UIKit
+
+class ScriptureTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var scriptureLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
